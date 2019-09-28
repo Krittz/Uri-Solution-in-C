34f184db6745d08e3812d4cf9aac1ae0e6f53ff3
Krittz <55900416+Krittz@users.noreply.github.com>
2019-09-28 08:09:36 -0300

    Create URI 1003.c

---
+++ URI 1003.c
@@ -0,0 +1,16 @@
+// URI 1003 SOLUTION IN C LANG
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int inteiro_a;
+    int inteiro_b;
+    int resultado_soma;
+    scanf("%d", &inteiro_a);
+    scanf("%d", &inteiro_b);
+    resultado_soma = inteiro_a + inteiro_b;
+    printf("SOMA = %d\n", resultado_soma);
+
+    
+}
