c9d5edde39c8fc3185aafeada5f9709c68b98253
Krittz <55900416+Krittz@users.noreply.github.com>
2019-09-28 08:16:11 -0300

    Create URI 1004.c

---
+++ URI 1004.c
@@ -0,0 +1,16 @@
+// URI 1004 SOLUTION IN C LANG
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int value_a;
+    int value_b;
+    int prod;
+        scanf("%d", &value_a);
+        scanf("%d", &value_b);
+    prod = value_a * value_b;
+
+    printf("PROD = %d\n", prod);
+
+}
