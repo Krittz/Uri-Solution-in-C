a12fba0e3e02adade1b50c49e5686f836925b8b7
Krittz <55900416+Krittz@users.noreply.github.com>
2019-09-29 17:46:10 -0300

    Create URI 1007.c

---
+++ URI 1007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int a, b, c, d;
+    int result_dife;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+result_dife = (a*b) - (c*d);
+printf("DIFERENCA = %d\n", result_dife);
+
+
+}
