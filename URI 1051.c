670761dfcfe7609ea51e95d9457080d9c0644f6d
Krittz <55900416+Krittz@users.noreply.github.com>
2019-10-01 12:55:37 -0300

    Create URI 1051.c

---
+++ URI 1051.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void main(){
+    double salario, x;
+
+    scanf("%lf", &salario);
+
+if(salario<= 2000.0){
+    printf("Isento\n");
+}
+else if(salario <= 3000.0){
+    x = (salario - 2000.0) *0.08;
+    printf("R$ %.2lf\n", x);
+}
+else if(salario <=4500.0){
+    x = (salario - 3000.0) * 0.18 + (1000*0.08);
+    printf("R$ %.2lf\n", x);  
+}
+else {
+    x = (salario - 4500.0 ) * 0.28 + (1500*0.18) + (1000*0.08);
+    printf("R$ %.2lf\n", x);
+}
+
+
+}
