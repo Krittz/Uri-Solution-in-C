ad14e3fb77d1f021567fc2a2ea4f60b4463a1b47
Krittz <55900416+Krittz@users.noreply.github.com>
2019-10-01 13:38:09 -0300

    Create URI 1113.c

---
+++ URI 1113.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    while(x!=y)
+    {
+        scanf("%d", &x);
+        scanf("%d", &y);
+        if(x > y)
+            printf("Decrescente\n");
+        else if(x<y)
+            printf("Crescente\n");
+    }
+    return 0;
+}
